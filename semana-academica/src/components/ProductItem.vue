b14a18826b06e70625eeb1573689fed5446d37ec
Luisa <luisastellet@id.uff.br>
2023-10-17 19:02:22 -0300

    .

---
+++ semana-academica/src/components/ProductItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <span>Brownie - 4 reais</span>
+    <div>
+        <button @click="diminuiContador">-</button>
+        <span>{{ contador }}</span>
+        <button @click="aumentaContador">+</button>
+    </div>
+</template>
+<script setup>
+import { ref } from 'vue'
+
+const contador = ref(0);
+const props = defineProps(['productName', 'productPrice'])
+
+const diminuiContador = {} => {
+    if(contador.value > 0) contador.value--;
+}
+const aumentaConador = {} => contador.value++;
+</script>
---
+++ semana-academica/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import ProductItem from '@/components/ProductItem.vue'
+
+const nomeUsuario = ref('');
+</script>
+
+<template>
+  <main>
+    <div>
+    <input type="text" v-model="nomeUsuario"/>
+    <p>Olá, {{ nomeUsuario }}</p>
+    </div>
+    <ProductItem productName="Brownie" productPrice="4"/>
+    <ProductItem productName="Guaravita" productPrice="2"/>
+    <ProductItem productName="Água" productPrice="2"/>
+  </main>
+</template>
